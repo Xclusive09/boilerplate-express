128972ad185f5e1a3d32425e38e6cb8eb2485f7c
Xclusive09 <101833166+Xclusive09@users.noreply.github.com>
2024-02-07 21:48:11 +0000

    upade

--- myApp.js
+++ myApp.js
@@ -2,11 +2,11 @@ let express = require("express");
 let app = express();
 const path = require("path");
 
-const absolutePath = path.join(__dirname, "views", "index.html");
+// const absolutePath = path.join(__dirname, "views", "index.html");
 
-app.get("/", (req, res) => {
-  res.sendFile(absolutePath);
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(absolutePath);
+// });
 
 const staticFiles = path.join(__dirname, "public");
 app.use("/public", express.static(staticFiles));
