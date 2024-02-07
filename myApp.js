639f621811752cd7e5bdfdf5d2b11dd364af0f59
Xclusive09 <101833166+Xclusive09@users.noreply.github.com>
2024-02-07 21:41:23 +0000

    serve public files

--- myApp.js
+++ myApp.js
@@ -8,6 +8,11 @@ app.get("/", (req, res) => {
   res.sendFile(absolutePath);
 });
 
+const staticFiles = path.join(__dirname, "public");
+app.use("public/", express.static(staticFiles));
+
+app.use(staticFiles);
+
 module.exports = app;
 
 module.exports = app;
