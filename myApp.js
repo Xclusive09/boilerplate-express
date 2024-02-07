2a3b2b21cfb3c356f5f459a30a2badc041df0ea3
Xclusive09 <101833166+Xclusive09@users.noreply.github.com>
2024-02-07 22:14:15 +0000

    json format

--- myApp.js
+++ myApp.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const publicDirectoryPath = path.join(__dirname, "public");
+// const publicDirectoryPath = path.join(__dirname, "public");
 
-app.use("/public", express.static(publicDirectoryPath));
+// app.use("/public", express.static(publicDirectoryPath));
+
+app.use('/json');
+
+ const handler (req, res)  => {
+  jsonData = {"message": "Hello json"}
+  res.json(jsonData);
+}
+app.get('/json', handler)
 
 module.exports = app;
