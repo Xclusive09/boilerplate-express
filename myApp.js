80026369d95de3178372d0ee280abd82d0fc7faa
Xclusive09 <101833166+Xclusive09@users.noreply.github.com>
2024-02-07 22:22:50 +0000

    update

--- myApp.js
+++ myApp.js
@@ -2,15 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-// const publicDirectoryPath = path.join(__dirname, "public");
-
-// app.use("/public", express.static(publicDirectoryPath));
-
 const publicRepo = path.join(__dirname, "public");
-app.use("/json"), express.static(publicRepo);
+
+app.use("/json", express.static(publicRepo));
 
 const handler = (req, res) => {
-  jsonData = { message: "Hello json" };
+  const jsonData = { message: "Hello json" };
+
   res.json(jsonData);
 };
 app.get("/json", handler);
